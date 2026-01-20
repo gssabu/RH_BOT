4e51e1935386905e7ea6af3304f2f1310284cfac
gssabu <87628438+gssabu@users.noreply.github.com>
2026-01-20 12:00:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ def cmd_sma_bot(a):
             atr_window=a.atr_window,
             rsi_window=a.rsi_window,
             trend_window=a.trend,
+            coin_limits = limits.get(args.symbol, {}) if 'limits' in globals() else {}
             mb = coin_limits.get("max_buy_price"),
             ms = coin_limits.get("min_sell_price")
         
@@ -214,6 +215,7 @@ def build():
 if __name__ == "__main__":
     args = build().parse_args()
     args.func(args)
+
 
 
 
